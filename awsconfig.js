6ca80fe788a302659ab57c939e93d49622cd4321
Lachlan Glen <lachlanjglen@gmail.com>
2020-04-16 19:08:33 -0400

    commit

--- awsconfig.js
+++ awsconfig.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
-const bucketName = 'duette';
-const bucketRegion = 'us-east-2';
-const identityPoolId = 'us-east-2:3e1253a3-a90c-43af-ba82-0ed847f70b39';
+const bucketName = process.env.AWS_BUCKET_NAME;
+const bucketRegion = process.env.AWS_BUCKET_REGION;
+const identityPoolId = process.env.AWS_IDENTITY_POOL_ID;
 
 AWS.config.update({
   region: bucketRegion,
--- server/api/aws/config.js
+++ server/api/aws/config.js
@@ -1,7 +1,4 @@
 const AWS = require('aws-sdk');
-const bucketName = process.env.AWS_BUCKET_NAME;
-const bucketRegion = process.env.AWS_BUCKET_REGION;
-const identityPoolId = process.env.AWS_IDENTITY_POOL_ID;
 
 AWS.config.update({
   region: bucketRegion,
