0282150634e00ee3f552bbaa528d2d1932da96e3
Lachlan Glen <lachlanjglen@gmail.com>
2020-06-17 12:23:02 -0400

    working on app store receipt verification route

--- server/api/appStore.js
+++ server/api/appStore.js
@@ -11,8 +11,8 @@ router.post('/', (req, res, next) => {
   axios.post('https://sandbox.itunes.apple.com/verifyReceipt', receiptBody)
     .then(result => {
       // console.log('result: ', result.data);
-      console.log('latest_receipt_info: ', JSON.parse(result.data.latest_receipt_info));
-      res.status(200).send(JSON.parse(result.data.latest_receipt_info));
+      console.log('latest_receipt_info: ', JSON.parse(JSON.stringify(result.data.latest_receipt_info)));
+      res.status(200).send(JSON.parse(JSON.stringify(result.data.latest_receipt_info)));
     })
     .catch(e => {
       console.log('error verifying receipt: ', e);
