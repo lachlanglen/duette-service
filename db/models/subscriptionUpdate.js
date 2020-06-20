7015ecd48d5ea8b511fc5f1bf410d8631f9a24b4
Lachlan Glen <lachlanjglen@gmail.com>
2020-06-20 09:24:09 -0400

    removing uuid from subscriptionUpdate model

--- db/models/subscriptionUpdate.js
+++ db/models/subscriptionUpdate.js
@@ -2,11 +2,6 @@ const Sequelize = require('sequelize');
 const connection = require('../connection');
 
 const SubscriptionUpdate = connection.define('subscriptionUpdate', {
-  id: {
-    primaryKey: true,
-    type: Sequelize.UUID,
-    defaultValue: Sequelize.UUIDV4,
-  },
   json: {
     type: Sequelize.JSONB,
     allowNull: false,
