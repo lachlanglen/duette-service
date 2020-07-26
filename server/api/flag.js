2e88cd211c83d769e10e9820e3e8b26e07787780
Lachlan Glen <lachlanjglen@gmail.com>
2020-07-26 12:34:15 -0400

    added flag GET route

--- server/api/flag.js
+++ server/api/flag.js
@@ -5,7 +5,7 @@ const { Flag } = require('../../db');
 router.post('/:videoId', (req, res, next) => {
   const { videoId } = req.params;
   const { flaggingUserId, flaggedUserId } = req.body;
-  console.log('videoId: ', videoId, 'flaggingUserId: ', flaggingUserId, 'flaggedUserId: ', flaggedUserId)
+  // console.log('videoId: ', videoId, 'flaggingUserId: ', flaggingUserId, 'flaggedUserId: ', flaggedUserId)
   if (!flaggingUserId || !flaggedUserId) {
     return res.status(400).send('Request must include flaggingUserId and flaggedUserId.')
   }
@@ -15,14 +15,19 @@ router.post('/:videoId', (req, res, next) => {
     flaggedUserId,
   })
     .then(flag => {
-      // do something with new flag
-      console.log('flag created! ', flag);
+      // console.log('flag created! ', flag);
       res.status(201).send(flag);
     })
     .catch(e => {
-      console.log('Error creating new flag: ', e);
+      // console.log('Error creating new flag: ', e);
       res.status(400).send('Error creating new flag: ', e)
     })
+});
+
+router.get('/', (req, res, next) => {
+  Flag.findAll()
+    .then(flags => res.status(200).send(flags))
+    .catch(e => res.status(400).send('Error finding all flags: ', e))
 });
 
 module.exports = router; 
